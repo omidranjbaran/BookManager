e493a1be5e47f8440ae146ae52fc9db81dd5c63f
omid <omidrn58@gmail.com>
2025-03-13 19:08:45 +0330

    Initial commit

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,13 @@
-from django.conf.urls import url
+from django.urls import path, re_path
 from . import views
 
-app_name='catalog'
+app_name = 'catalog'
+
 urlpatterns = [
-    url(r'^$', views.Index.as_view(), name='index'),
-    url(r'^books/$', views.BookListView.as_view(), name='books'),
-    url(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(),name='book-detail'),
-    url(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
-    url(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
-    url(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
+    path('', views.Index.as_view(), name='index'),
+    path('books/', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    path('authors/', views.AuthorListView.as_view(), name='authors'),
+    path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+    re_path(r'^book/(?P<pk>[-\w]+)/renew/$', views.renew_book_librarian, name='renew-book-librarian'),
 ]
--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -1,29 +1,16 @@
-"""locallibrary URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.conf.urls import url, include
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    url(r'admin/', admin.site.urls),
-    url(r'catalog/', include('catalog.urls'))
+    path('admin/', admin.site.urls),
+    path('catalog/', include('catalog.urls')),
+    path('', include('catalog.urls')),  # افزودن مسیر صفحه اصلی (از catalog)
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
 urlpatterns += [
-    url(r'^accounts/', include('django.contrib.auth.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
